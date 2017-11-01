c70299ba4127f3c78ca0e458899fc1a98dbf2ef1
alenatolcheeva <atolchieieva@bk.ru>
2017-11-01 11:23:40 +0300

    message

--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.WriteLine("hello 123");
         }
     }
 }
